17d16330e00af40d5ef2b2999616c443ad50c06b
Rishabh Singh <ris8645208@gmail.com>
2021-07-01 21:36:36 +0530

    added my skills and my works section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,47 @@
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
+$(document).ready(function () {
+  $(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip({
+          container:"body"
     });
 
-    // Scrolling Effect
-$('a').click(function(){
-      // applying again smooth scroll on menu items click
-      $('html').css("scrollBehavior", "smooth");
+  $('[data-toggle="tooltip"]').tooltip({
+    container: "body",
   });
 
-  var typed = new Typed(".typing-2", {
-      strings: ["Student","Frontend Developer", "Freelancer", "Content Creator"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true
+  $(".menu-icon").on("click", function () {
+    $("nav ul").toggleClass("showing");
   });
 
+  $(".menu-icon").click(function () {
+    $(".menu-icon").toggleClass("showing");
+  });
 
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
+  // Scrolling Effect
+  $("a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  var typed = new Typed(".typing", {
+    strings: ["Student", "Frontend Developer", "Freelancer", "Content Creator"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+      $("nav").addClass("black");
+    } else {
+      $("nav").removeClass("black");
     }
 
-    else {
-          $('nav').removeClass('black');
+    // scroll-up button show/hide script
+    if (this.scrollY > 300) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
     }
-
-     // scroll-up button show/hide script
-     if(this.scrollY > 300){
-            $('.scroll-up-btn').addClass("show");
-      }
-      else{
-            $('.scroll-up-btn').removeClass("show");
-      }
+  });
 })
-
 });
